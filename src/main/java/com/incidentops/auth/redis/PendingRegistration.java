2bc99fed98756bca8a8f706c61b2e1d9a2b28af7
ArjunRai1 <arjunrai2214@gmail.com>
2026-07-04 21:24:30 +0530

    Configured OTP Generator

---
+++ src/main/java/com/incidentops/auth/redis/PendingRegistration.java
@@ -0,0 +1,18 @@
+package com.incidentops.auth.redis;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PendingRegistration {
+    private String username;
+    private String email;
+    private String hashedPassword;
+    private String otp;
+    private String createdAt;
+}

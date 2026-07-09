bf66b4f8f170d79cf7f9799316c760ec62288263
ArjunRai1 <arjunrai2214@gmail.com>
2026-07-09 12:50:45 +0530

    Fixed DB issues

--- src/main/resources/db/migration/V3__incident-comments.sql
+++ src/main/resources/db/migration/V3__incident-comments.sql
@@ -7,7 +7,7 @@ CREATE TABLE COMMENTS(
     updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     constraint fk_comments_user_id foreign key (user_id) references users(id),
-    constraint fk_comments_incident_id foreign key (incident_id) references incidents(id);
+    constraint fk_comments_incident_id foreign key (incident_id) references incidents(id)
 );
 
-CREATE INDEX idx_comments_incident_created_at ON incident_comments (incident_id, created_at);
+CREATE INDEX idx_comments_incident_created_at ON COMMENTS (incident_id, created_at);

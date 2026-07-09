6d675b482b81c83c5f53d2c990c8e24b4bc32e2a
ArjunRai1 <arjunrai2214@gmail.com>
2026-07-09 11:28:38 +0530

    Comments schema implementation

---
+++ src/main/resources/db/migration/V3__incident-comments.sql
@@ -0,0 +1,13 @@
+CREATE TABLE COMMENTS(
+    id bigserial primary key,
+    incident_id bigint not null,
+    user_id bigint not null,
+    comment text not null,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    constraint fk_comments_user_id foreign key (user_id) references users(id),
+    constraint fk_comments_incident_id foreign key (incident_id) references incidents(id);
+);
+
+CREATE INDEX idx_comments_incident_created_at ON incident_comments (incident_id, created_at);
